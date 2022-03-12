7e962dc25bf5f0b4143d58b914c06e29cd4c65a6
gifuitvnluan <46041092+gifuitvnluan@users.noreply.github.com>
2022-03-12 10:28:06 +0700

    ss

--- blogs.php
+++ blogs.php
@@ -18,7 +18,7 @@
 <!-- navbar section starts  -->
 
 <nav class="navbar">
-    <a href="home.php"> <i class="fas fa-home"></i> <span>home</span> </a>
+    <a href="./"> <i class="fas fa-home"></i> <span>home</span> </a>
     <a href="about.php"> <i class="fas fa-user"></i> <span>about</span> </a>
     <a href="portfolio.php"> <i class="fas fa-briefcase"></i> <span>portfolio</span> </a>
     <a href="blogs.php"> <i class="fas fa-blog"></i> <span>blogs</span> </a>
