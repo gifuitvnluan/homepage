ec38f5e9b998e2eb752dca051299287d2c10b427
gifuitvnluan <46041092+gifuitvnluan@users.noreply.github.com>
2022-03-28 08:26:20 +0700

    test

---
+++ index.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var app = express();
+
+app.use(express.static("public"));
+
+app.set('view engine', 'ejs');
+app.set("views","./views");
+
+
+app.listen(8888, function() {
+    console.log("đã mở cổng");
+})
+
+// All router
+app.get('/', function(req, res) {
+    res.render('index');
+})
