ce515f36b4bba151276f8950087cd759e8ceed75
Nguyễn Thành Luân <46041092+gifuitvnluan@users.noreply.github.com>
2025-08-21 17:03:29 +0700

    ss

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,8 +1,24 @@
+"use client"
+
 import Image from "next/image";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faUser } from '@fortawesome/free-solid-svg-icons'
+import { faDownload } from '@fortawesome/free-solid-svg-icons'
+import { useEffect, useState } from "react";
 
 export default function About() {
+  const [age, setAge] = useState(0);
+  const [experience, setExperience] = useState(0);
+
+  useEffect(() => {
+    var myYear = process.env.NEXT_PUBLIC_YEAR_BORN || 0;
+    var myStartYear = process.env.NEXT_PUBLIC_YEAR_WORK_START || 0;
+    var currentYear = new Date().getFullYear();
+
+    setAge((Number(currentYear) - (Number(myYear) || 0)));
+    setExperience((Number(currentYear) - (Number(myStartYear) || 0)));
+
+  }, []);
+
   return (
     <div  id="swup" className="transition-fade">
       <section className="about">
@@ -19,7 +35,7 @@ export default function About() {
 
                     <div className="box">
                         <h3> <span>name : </span> Nguyễn Thành Luân </h3>
-                        <h3> <span>age : </span> <span className="span_age" style={{ color: 'inherit', fontWeight: 'inherit'}}></span> </h3>
+                        <h3> <span>age : </span> <span className="span_age" style={{ color: 'inherit', fontWeight: 'inherit'}}>{age}</span> </h3>
                         <h3> <span>email : </span> nguyenthanhluan995@gmail.com </h3>
                         <h3> <span>address : </span> Quy Nhon - Binh Dinh, Viet Nam </h3>
                     </div>
@@ -27,20 +43,20 @@ export default function About() {
                     <div className="box">
                         <h3> <span>freelance : </span> available </h3>
                         <h3> <span>skill : </span> front-end </h3>
-                        <h3> <span>experience : </span> <span className="span_experience" style={{ color: 'inherit', fontWeight: 'inherit'}}></span> years </h3>
+                        <h3> <span>experience : </span> <span className="span_experience" style={{ color: 'inherit', fontWeight: 'inherit'}}>{experience}</span> years </h3>
                         <h3> <span>language : </span> Vietnamese </h3>
                     </div>
 
                 </div>
 
-                <a href="./files/nguyen-thanh-luan_cveng.pdf" className="btn" download> download CV <i className="fas fa-download"></i> </a>
+                <a href="./files/nguyen-thanh-luan_cveng.pdf" className="btn" download> download CV <i><FontAwesomeIcon icon={faDownload} /></i></a>
 
             </div>
 
             <div className="count-container">
 
                 <div className="box">
-                    <h3><span className="span_experience"></span>+</h3>
+                    <h3><span className="span_experience">{experience}</span>+</h3>
                     <p>years of experience</p>
                 </div>
 
@@ -70,51 +86,51 @@ export default function About() {
 
         <div className="box-container">
             <div className="box">
-                <Image width={100} height={100} src="images/icon-1.png"/>
+                <Image width={62} height={70} src="/images/icon-1.png" alt="html"/>
                 <h3>html</h3>
             </div>
             <div className="box">
-                <Image width={100} height={100} src="images/icon-2.png"/>
+                <Image width={63} height={70} src="/images/icon-2.png" alt="css"/>
                 <h3>css</h3>
             </div>
             <div className="box">
-                <Image width={100} height={100} src="images/icon-3.png"/>
+                <Image width={61} height={70} src="/images/icon-3.png" alt="javascript"/>
                 <h3>javascript</h3>
             </div>
             <div className="box">
-                <Image width={100} height={100} src="images/icon-4.png"/>
+                <Image width={95} height={70} src="/images/icon-4.png" alt="sass"/>
                 <h3>sass</h3>
             </div>
             <div className="box">
-                <Image width={100} height={100} src="images/icon-5.png"/>
+                <Image width={73} height={70} src="/images/icon-5.png" alt="jquery"/>
                 <h3>jquery</h3>
             </div>
             <div className="box">
-                <Image width={100} height={100} src="images/icon-6.png"/>
+                <Image width={67} height={70} src="/images/icon-6.png" alt="laravel"/>
                 <h3>laravel</h3>
             </div>
             <div className="box">
-                <Image width={100} height={100} src="images/icon-10.png"/>
+                <Image width={130} height={70} src="/images/icon-10.png" alt="php"/>
                 <h3>php</h3>
             </div>
             <div className="box">
-                <Image width={100} height={100} src="images/icon-7.png"/>
+                <Image width={70} height={70} src="/images/icon-7.png" alt="wordpress"/>
                 <h3>WordPress</h3>
             </div>
             <div className="box">
-                <Image width={100} height={100} src="images/icon-8.png"/>
+                <Image width={70} height={70} src="/images/icon-8.png" alt="adobe-photoshop"/>
                 <h3>Adobe Photoshop</h3>
             </div>
             <div className="box">
-                <Image width={100} height={100} src="images/icon-9.png"/>
+                <Image width={70} height={70} src="/images/icon-9.png" alt="adobe-illustrator"/>
                 <h3>Adobe Illustrator</h3>
             </div>
             <div className="box">
-                <Image width={100} height={100} src="images/icon-11.png"/>
+                <Image width={70} height={70} src="/images/icon-11.png" alt="adobe-animate"/>
                 <h3>Adobe Animate</h3>
             </div>
             <div className="box">
-                <Image width={100} height={100} src="images/icon-12.png"/>
+                <Image width={127} height={70} src="/images/icon-12.png" alt="asterisk"/>
                 <h3>Asterisk <br/>(Call Center)</h3>
             </div>
         </div>
