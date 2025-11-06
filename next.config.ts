515d9d15f56228bc379822bc9cce733fb6a9fafe
gifuitvnluan <46041092+gifuitvnluan@users.noreply.github.com>
2025-11-06 11:12:22 +0700

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,11 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === 'production'
+
 const nextConfig: NextConfig = {
   // add static
   output: 'export',
+  basePath: isProd ? '/homepage' : '',
   /* config options here */
   // Add remote patterns for next/image
   images: {
